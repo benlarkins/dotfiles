5b00836bae665519d76f10d11d2889f9de289f7a
Ben Larkins <benlarkins@pm.me>
2022-04-30 17:53:52 -0600

    Update wireless-bars.sh

--- xmobar/scripts/wireless-bars.sh
+++ xmobar/scripts/wireless-bars.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/sh
 
-iwconfig wlp0s20f0u4 | grep -q no\ wireless\ extensions\. && {
+iwconfig wlp4s0 | grep -q no\ wireless\ extensions\. && {
   echo wired
   exit 0
 }
 
-essid=`iwconfig wlp0s20f0u4 | awk -F '"' '/ESSID/ {print $2}'`
-stngth=`iwconfig wlp0s20f0u4 | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1`
+essid=`iwconfig wlp4s0 | awk -F '"' '/ESSID/ {print $2}'`
+stngth=`iwconfig wlp4s0 | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1`
 bars=`expr $stngth / 25`
 
 case $bars in
-  0)  bar='' ;;
-  1)  bar='' ;;
-  2)  bar='' ;;
-  3)  bar='' ;;
+  0)  bar='' ;;
+  1)  bar='' ;;
+  2)  bar='' ;;
+  3)  bar='' ;;
   4)  bar='' ;;
   *)  bar='' ;;
 esac
