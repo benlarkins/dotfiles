d931f9ea81b87b6787e56c89fe9587af5812991c
Ben Larkins <benlarkins@pm.me>
2022-08-23 15:38:11 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ cp -r ~/.dotfiles/.gitpod/.config /home/gitpod
 cp -r ~/.dotfiles/.gitpod/.node-version /home/gitpod
 cp -r ~/.dotfiles/.gitpod/.tmux.conf /home/gitpod
 sudo chsh -s /usr/bin/fish
-fisher install /home/gitpod/.config/fish/fish_plugins
+/usr/bin/fish -c 'fisher install /home/gitpod/.config/fish/fish_plugins'
 
