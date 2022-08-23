a1c5262c7a3de557a275250460a170816d37027f
Ben Larkins <benlarkins@pm.me>
2022-08-23 15:42:34 -0600

    Change shell

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 cp -r ~/.dotfiles/.gitpod/.config /home/gitpod
 cp -r ~/.dotfiles/.gitpod/.node-version /home/gitpod
 cp -r ~/.dotfiles/.gitpod/.tmux.conf /home/gitpod
-sudo chsh -s /usr/bin/fish
+sudo chsh -s /usr/bin/fish gitpod
 /usr/bin/fish -c 'fisher install /home/gitpod/.config/fish/fish_plugins'
 
