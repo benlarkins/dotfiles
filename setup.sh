6cb3d35692cbf3efc8790e42b71b9d559b4313d1
Ben Larkins <benlarkins@pm.me>
2022-08-24 12:19:04 -0600

    Run nvim headless

--- setup.sh
+++ setup.sh
@@ -16,5 +16,5 @@ curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --c
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 /usr/bin/fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
 /usr/bin/fish -c 'fisher install /home/gitpod/.config/fish/fish_plugins'
-nvim +'PlugInstall --sync' +qa
+nvim --headless +PlugInstall +qall
 
